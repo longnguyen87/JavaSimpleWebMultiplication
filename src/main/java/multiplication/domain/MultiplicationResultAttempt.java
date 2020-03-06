e040ba041a1addb6cb274a0d07541216953c7012
Long Nguyen <nguyenhlongvn@gmail.com>
2020-03-05 18:32:35 -0600

    Finish domain classes.

--- src/main/java/multiplication/domain/MultiplicationResultAttempt.java
+++ src/main/java/multiplication/domain/MultiplicationResultAttempt.java
@@ -1,5 +1,23 @@
 package multiplication.domain;
 
-public class MultiplicationResultAttempt {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class MultiplicationResultAttempt {
+	private final User user;
+	private final Multiplication multiplication;
+	private final int resultAttempt;
+	
+	public MultiplicationResultAttempt() {
+		// TODO Auto-generated constructor stub
+		user = null;
+		multiplication = null;
+		resultAttempt = -1;
+	}
 }
--- src/main/java/multiplication/domain/User.java
+++ src/main/java/multiplication/domain/User.java
@@ -9,7 +9,7 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
-public class User {
+public final class User {
 	private final String alias;
 	// Empty constructor for JSON (de)serialization
 	protected User() {
