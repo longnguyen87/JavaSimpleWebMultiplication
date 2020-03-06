38af62369f6efeb1213985935281b15c321f69a8
Long Nguyen <nguyenhlongvn@gmail.com>
2020-03-05 21:16:52 -0600

    Adding logic to MulController class. Junit passed.

--- src/main/java/multiplication/controller/MultiplicationController.java
+++ src/main/java/multiplication/controller/MultiplicationController.java
@@ -1,7 +1,11 @@
 package multiplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import multiplication.domain.Multiplication;
 import multiplication.service.MultiplicationService;
 
 /*
@@ -11,14 +15,20 @@ import multiplication.service.MultiplicationService;
  * 3. return Boolean result when user check attempt
  */
 @RestController
+@RequestMapping("/multiplications")
 public class MultiplicationController {
 	
 	private final MultiplicationService service;
 	// Using autowired to inject dependency
-
+	
+	@Autowired
 	public MultiplicationController(final MultiplicationService service) {
 		super();
 		this.service = service;
 	}
-	
+	@GetMapping("/random")
+	public Multiplication getRandomMultiplication () {
+		// this method will return a Multiplication object
+		return service.createRandomMultiplication();
+	}
 }
