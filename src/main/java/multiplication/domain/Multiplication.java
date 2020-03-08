22e57553fc2563e5362202cfa62f4971f05f3498
Long Nguyen <nguyenhlongvn@gmail.com>
2020-03-07 18:10:09 -0600

    Adding JPA entity to Multiplication class. Page 82

--- src/main/java/multiplication/domain/Multiplication.java
+++ src/main/java/multiplication/domain/Multiplication.java
@@ -1,5 +1,10 @@
 package multiplication.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +17,17 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class Multiplication {
+	@Id
+	@GeneratedValue
+	@Column(name = "MULTIPLICATION_ID")
+	private Long id;
 	private final int factor1;
 	private final int factor2;
 
 	Multiplication() {
-		this(0,0);
+		this(0, 0);
 	}
-	
+
 }
